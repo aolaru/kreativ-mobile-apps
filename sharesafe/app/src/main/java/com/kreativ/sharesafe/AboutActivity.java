29f09a04175ea2409f2ef32a184f0b5dea2d4f70
andreiolaru <andrei.olaru@gmail.com>
2026-09-16 00:08:35 +0300

    Link ShareSafe to dedicated privacy policy

--- sharesafe/app/src/main/java/com/kreativ/sharesafe/AboutActivity.java
+++ sharesafe/app/src/main/java/com/kreativ/sharesafe/AboutActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 
 /** Plain-language privacy and support information for ShareSafe. */
 public class AboutActivity extends Activity {
-    private static final String PRIVACY_POLICY_URL = "https://kreativfont.com/blog/terms-of-use-privacy-policy";
+    private static final String PRIVACY_POLICY_URL = "https://madebykreativ.com/privacy/sharesafe/";
     private static final String SUPPORT_EMAIL = "info@madebykreativ.com";
 
     @Override
